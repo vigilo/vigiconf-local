5d27d7a9b58a8e30fbd1b43386bc8ebf891a4748
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2012-12-14 14:17:40 +0100

    Bump des versions
    Change-Id: I7ddc465b73f8642df476104ce4ec466bc8d13cbf
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1028
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def get_data_files():
 
 
 setup(name='vigilo-vigiconf-local',
-        version='3.0.0',
+        version='3.0.1',
         author='Vigilo Team',
         author_email='contact@projet-vigilo.org',
         url='http://www.projet-vigilo.org/',
