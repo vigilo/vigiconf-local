a1732b4d50c64f387c18fd808cf98f415668eb11
Francois POIROTTE <francois.poirotte@c-s.fr>
2021-01-28 16:50:21 +0100

    Update the copyrights
    Change-Id: Ic3e7149206be2b15b49138cdc7b31e60c97333cc

--- src/vigilo/vigiconf_local/__init__.py
+++ src/vigilo/vigiconf_local/__init__.py
@@ -1,6 +1,6 @@
 # vim:set expandtab tabstop=4 shiftwidth=4:
 # -*- coding: utf-8 -*-
-# Copyright (C) 2010-2020 CS GROUP - France
+# Copyright (C) 2010-2021 CS GROUP - France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 """
